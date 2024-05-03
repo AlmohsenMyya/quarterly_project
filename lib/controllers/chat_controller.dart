b2eb53f81ea47a314404209ee2ad8475d98308f9
almohsen myya <almohsenmyya2233@@gmail.com>
2024-05-03 14:31:19 -0400

    fix and refactor chat_controller.dart

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -1,4 +1,8 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
@@ -13,13 +17,34 @@ class ChatController extends GetxController {
   final RxBool isUploading = false.obs;
   final RxList<Message> messages = <Message>[].obs;
 
+  // for authentication
+  static FirebaseAuth get auth => FirebaseAuth.instance;
+  // for accessing cloud firestore database
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  // for accessing firebase storage
+  static FirebaseStorage storage = FirebaseStorage.instance;
+
   ChatController(this.repository);
+  static User get my_account => auth.currentUser!;
+  static late ChatUser me; // for storing self information
 
   @override
   void onInit() {
     super.onInit();
     // Fetch messages from the repository
     fetchMessages();
+    me = ChatUser(
+        id: my_account.uid,
+        name: my_account.displayName.toString(),
+        email: my_account.email.toString(),
+        about: "Hey, I'm using We Chat!",
+        image: my_account.photoURL.toString(),
+        createdAt: '',
+        isOnline: false,
+        lastActive: '',
+        pushToken: '');
   }
 
   void fetchMessages() {
@@ -30,17 +55,19 @@ class ChatController extends GetxController {
 
   void sendMessage() {
     final messageText = textController.text;
-    ChatRepository.sendMessage(repository.user,messageText ,Type.text );
+    ChatRepository.sendMessage(repository.user, messageText, Type.text);
     textController.text = '';
   }
 
   void toggleEmoji() {
+    print("${showEmoji.value}");
     showEmoji.value = !showEmoji.value;
   }
 
   Future<void> pickImage(ImageSource source) async {
     final picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: source, imageQuality: 70);
+    final XFile? image =
+        await picker.pickImage(source: source, imageQuality: 70);
 
     if (image != null) {
       isUploading.value = true;
@@ -48,5 +75,31 @@ class ChatController extends GetxController {
       await repository.sendImage(file);
       isUploading.value = false;
     }
+  }
+
+  // useful for getting conversation id
+  static String getConversationID(String id) =>
+      my_account.uid.hashCode <= id.hashCode
+          ? '${my_account.uid}_$id'
+          : '${id}_${my_account.uid}';
+
+  //delete message
+  static Future<void> deleteMessage(Message message) async {
+    await firestore
+        .collection('chats/${getConversationID(message.toId)}/messages/')
+        .doc(message.sent)
+        .delete();
+
+    if (message.type == Type.image) {
+      await storage.refFromURL(message.msg).delete();
+    }
+  }
+
+  //update message
+  static Future<void> updateMessage(Message message, String updatedMsg) async {
+    await firestore
+        .collection('chats/${getConversationID(message.toId)}/messages/')
+        .doc(message.sent)
+        .update({'msg': updatedMsg});
   }
 }
